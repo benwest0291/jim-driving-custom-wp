40f82e5edf013947011ce2faadd6e78904aabb5d
Ben West <benwest@Bens-MacBook-Pro.local>
2023-08-28 16:25:04 +0100

    styling changes

--- template-about.php
+++ template-about.php
@@ -16,7 +16,7 @@ render_banner('about_banner');?>
 
 <section class="about">
     <div class="container">
-        <div class="row">
+        <div class="row mb-2 mb-md-4">
             <div class="col-12 col-lg-6">
                 <h6 class="about__subheading mt-4 mb-2"><?php echo $subheading; ?></h6>
                 <?php if($heading == null){ ?>
@@ -30,7 +30,7 @@ render_banner('about_banner');?>
                         <?php echo $content; ?>
                     </div>
                 <?php } ?>
-                <div class="mt-4 mb-2">
+                <div class=" mb-2">
                     <?php if ($button_one_text != null && $button_one_url != null){ ?>
                         <a title="<?php echo $button_one_text;?>" class="btn__primary" href="<?php echo $button_one_url;?>"><?php echo $button_one_text;?></a>
                     <?php } ?>
